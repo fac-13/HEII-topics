dac69b44f2ce38bd773bf910bf0f9b93083061d0
eadehemingway <eade.hemingway@gmail.com>
2018-04-05 16:11:42 +0100

    refactor router

--- src/router.js
+++ src/router.js
@@ -1,45 +1,25 @@
-const { getData, postData } = require('./dynamic');
-const { staticHandler, getDataHandler } = require('./handler');
-const querystring = require('querystring');
+const { getData, postData } = require("./dynamic");
+const { staticHandler, getDataHandler, postDataHandler } = require("./handler");
+const querystring = require("querystring");
 
 const router = (request, response) => {
   const url = request.url;
 
-  if (url === '/') {
-    staticHandler(response, '/public/index.html');
-  } else if (url.indexOf('public') !== -1) {
+  if (url === "/") {
+    staticHandler(response, "/public/index.html");
+  } else if (url.indexOf("public") !== -1) {
     staticHandler(response, url);
-  } else if (url.indexOf('get/topics') !== -1) {
+  } else if (url.indexOf("get/topics") !== -1) {
     getDataHandler(
       response,
-      'SELECT topic.name, topic.description, users.username FROM topic, users WHERE topic.user_id = users.id'
+      "SELECT topic.name, topic.description, users.username FROM topic, users WHERE topic.user_id = users.id"
     );
-  } else if ('/create-topic') {
+  } else if ("/create-topic") {
     // postData goes here
-    let body = '';
-    request.on('data', chunk => (body += chunk));
-    request.on('end', () => {
-      const data = querystring.parse(body);
-      console.log('parsed data: ', data);
-      const topicTitle = data.topic_title;
-      const description = data.description;
-      postData(topicTitle, description, (err, res) => {
-        if (err) {
-          console.log(err);
-          response.writeHead(303, { Location: '/' });
-          response.writeHead(500, { 'content-type': 'text/plain' });
-          response.end('Something went wrong');
-        } else {
-          console.log('reached');
-          response.writeHead(200, { 'content-type': 'text/plain' });
-          response.writeHead(303, { Location: '/' });
-          response.end(`Successfully added ${topicTitle}`);
-        }
-      });
-    });
+    postDataHandler(request, response);
   } else {
-    response.writeHead(404, { 'content-type': 'text/plain' });
-    response.end('404 error');
+    response.writeHead(404, { "content-type": "text/plain" });
+    response.end("404 error");
   }
 };
 
