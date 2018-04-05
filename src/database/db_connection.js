f1d00cf0fa7bddcfdd47fd68486a94ca9e5da06c
i2xzy <yitzchokr@live.com>
2018-04-05 15:46:27 +0100

    add get/topics route and handler and tests closes #3

--- src/database/db_connection.js
+++ src/database/db_connection.js
@@ -1,26 +1,27 @@
 const { Pool } = require('pg');
 const url = require('url');
 
-require('env2')('./config.env');
+require('env2')('./.env');
 
 let DB_URL = process.env.DB_URL;
-if (process.env.NODE_ENV === "test") {
-    DB_URL = process.env.TEST_DB_URL;
+if (process.env.NODE_ENV === 'test') {
+  DB_URL = process.env.TEST_DB_URL;
 }
 
-if (!process.env.DB_URL) throw new Error('Environment variable DB_URL must be set');
+if (!process.env.DB_URL)
+  throw new Error('Environment variable DB_URL must be set');
 
 const params = url.parse(process.env.DB_URL);
 const [username, password] = params.auth.split(':');
 
 const options = {
-    host: params.hostname,
-    port: params.port,
-    database: params.pathname.split('/')[1],
-    max: process.env.DB_MAX_CONNECTIONS || 2,
-    user: username,
-    password,
-    ssl: params.hostname !== 'localhost'
+  host: params.hostname,
+  port: params.port,
+  database: params.pathname.split('/')[1],
+  max: process.env.DB_MAX_CONNECTIONS || 2,
+  user: username,
+  password,
+  ssl: params.hostname !== 'localhost'
 };
 
 module.exports = new Pool(options);
--- src/router.js
+++ src/router.js
@@ -1,31 +1,23 @@
 const { getData, postData } = require('./dynamic');
-const { staticHandler } = require('./handler');
+const { staticHandler, getDataHandler } = require('./handler');
 
 const router = (request, response) => {
   const url = request.url;
 
-  if (url === "/") {
-    staticHandler(response, "/public/index.html");
-  } else if (url.indexOf("public") !== -1) {
+  if (url === '/') {
+    staticHandler(response, '/public/index.html');
+  } else if (url.indexOf('public') !== -1) {
     staticHandler(response, url);
-  } else if ("/topics") {
-    console.log("search router reached");
-    getData((err, res) => {
-        if (err) {
-            console.log(err);
-            response.writeHead(500, {'content-type': 'text/plain'});
-            response.end('server error');
-        } else {
-            let output = JSON.stringify(res);
-            response.writeHead(200, {'content-type': 'application/json'});
-            response.end(output);
-        }
-    });
-  } else if ("/create-topic") {
+  } else if (url.indexOf('get/topics') !== -1) {
+    getDataHandler(
+      response,
+      'SELECT topic.name, topic.description, users.username FROM topic, users WHERE topic.user_id = users.id'
+    );
+  } else if ('/create-topic') {
     // postData goes here
   } else {
-    response.writeHead(404, { "content-type": "text/plain" });
-    response.end("404 error");
+    response.writeHead(404, { 'content-type': 'text/plain' });
+    response.end('404 error');
   }
 };
 
