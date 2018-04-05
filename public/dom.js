ed9b293108d1fccb7b5bb22b49824f45cad03185
i2xzy <yitzchokr@live.com>
2018-04-05 16:25:34 +0100

    refactor front-end

--- public/dom.js
+++ public/dom.js
@@ -1,22 +1,6 @@
-//   -- FETCH requests
-var request = function (url, callback) {
-  var xhr = new XMLHttpRequest();
-
-  xhr.addEventListener("load", function () {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      console.log("fetch is working", url);
-      var response = JSON.parse(xhr.responseText);
-      callback(response);
-    } else {
-      console.log("XHR error", xhr.readyState);
-    }
-  });
-  xhr.open("GET", url, true);
-  xhr.send();
-};
-
-// -- EVENT LISTENERS
-// remember to add prevent default to submit button
+(function() {
+  var updateDom = function() {};
+})();
 
 // -- CALLBACK FUNCTIONS
 // dom manipulation to update after receiving data
