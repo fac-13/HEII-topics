c4116c1838f5e18f7f082704aa3cddab9f0a3e60
i2xzy <yitzchokr@live.com>
2018-04-11 16:30:07 +0100

    change db_connection to use DB_URL var relates #24

--- src/database/db_connection.js
+++ src/database/db_connection.js
@@ -15,10 +15,9 @@ if (process.env.TRAVIS === 'true') {
     DB_URL = process.env.TEST_DB_URL;
   }
 
-  if (!process.env.DB_URL)
-    throw new Error('Environment variable DB_URL must be set');
+  if (!DB_URL) throw new Error('Environment variable DB_URL must be set');
 
-  const params = url.parse(process.env.DB_URL);
+  const params = url.parse(DB_URL);
   const [username, password] = params.auth.split(':');
 
   options = {
